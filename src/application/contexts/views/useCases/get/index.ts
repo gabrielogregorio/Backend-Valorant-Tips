5b0c837947951b4a0edd509daec89d6e9c819b8e
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2024-12-19 16:20:06 -0300

    fix: remain case private repository

--- src/application/contexts/views/useCases/get/index.ts
+++ src/application/contexts/views/useCases/get/index.ts
@@ -2,11 +2,11 @@ import { ViewsRepositoryInterface } from '@/domain/contexts/contexts/views/repos
 import { GetViewUseCaseInterface, GetViewsOutputDtoInterface } from './GetViewUseCaseInterface';
 
 export class GetViewUseCase implements GetViewUseCaseInterface {
-  constructor(private viewRepository: ViewsRepositoryInterface) {}
+  constructor(private _viewRepository: ViewsRepositoryInterface) {}
 
   execute = async (): Promise<GetViewsOutputDtoInterface> => {
-    const count2 = await this.viewRepository.findAllDistinctIp();
-    const count = await this.viewRepository.findAll();
+    const count2 = await this._viewRepository.findAllDistinctIp();
+    const count = await this._viewRepository.findAll();
     const countAll = count.length;
     const countIps = count2.length;
 
