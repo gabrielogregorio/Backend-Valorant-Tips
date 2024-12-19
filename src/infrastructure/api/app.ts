e5b3fea86d88cbc80e9c1676662b824f934f4256
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2024-12-19 16:12:27 -0300

    feat: remove crossorigin helmet

--- src/infrastructure/api/app.ts
+++ src/infrastructure/api/app.ts
@@ -18,7 +18,7 @@ app.use(useLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(useSanitizeMongo);
 app.use(useIpRequestLimiter);
 
