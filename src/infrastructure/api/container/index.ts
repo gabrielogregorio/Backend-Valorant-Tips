0a96ad87ae333792f3bf1f2efb2a82dc584ea7ed
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2024-12-20 10:47:24 -0300

    feat: create post tag

--- src/infrastructure/api/container/index.ts
+++ src/infrastructure/api/container/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import { CodeRepositoryInterface } from '@/domain/contexts/contexts/code/repository';
 import { PostRepositoryInterface } from '@/domain/contexts/contexts/post/repository';
 import { SuggestionRepositoryInterface } from '@/domain/contexts/contexts/suggestion/repository';
@@ -76,6 +77,30 @@ import { GetMapsUseCaseInterface } from '@/application/contexts/maps/useCases/ge
 import { GetMapsUseCase } from '@/application/contexts/maps/useCases/get';
 import { MapsControllerInterface } from '@/infrastructure/api/controllers/interfaces/MapsControllerInterface';
 import { MapsController } from '@/infrastructure/api/controllers/mapsController';
+import { CreateAgentUseCase } from '@/application/contexts/agents/useCases/add';
+import { CreateAgentUseCaseInterface } from '@/application/contexts/agents/useCases/add/CreateAgentUseCaseInterface';
+import { AgentsRepository } from '@/infrastructure/contexts/agents/repository/mongo/agentsRepository';
+import { AgentsRepositoryInterface } from '@/domain/contexts/contexts/agents/repository';
+import { GetAgentsUseCaseInterface } from '@/application/contexts/agents/useCases/get/GetAgentsUseCaseInterface';
+import { GetAgentsUseCase } from '@/application/contexts/agents/useCases/get';
+import { AgentsControllerInterface } from '@/infrastructure/api/controllers/interfaces/AgentsControllerInterface';
+import { AgentsController } from '@/infrastructure/api/controllers/agentsController';
+import { PostTagCategoryRepositoryInterface } from '@/domain/contexts/contexts/postTagCategory/repository';
+import { PostTagCategoryRepository } from '@/infrastructure/contexts/postTagCategory/repository/mongo/postTagCategoryRepository';
+import { CreatePostTagCategoryUseCase } from '@/application/contexts/postTagCategory/useCases/add';
+import { GetPostTagCategoryUseCaseInterface } from '@/application/contexts/postTagCategory/useCases/get/GetPostTagCategoryUseCaseInterface';
+import { GetPostTagCategoryUseCase } from '@/application/contexts/postTagCategory/useCases/get';
+import { PostTagCategoryController } from '@/infrastructure/api/controllers/postTagCategoryController';
+import { PostTagCategoryControllerInterface } from '@/infrastructure/api/controllers/interfaces/PostTagCategoryControllerInterface';
+import { PostTagsRepositoryInterface } from '@/domain/contexts/contexts/postTags/repository';
+import { PostTagsRepository } from '@/infrastructure/contexts/postTags/repository/mongo/postTagsRepository';
+import { CreatePostTagsUseCaseInterface } from '@/application/contexts/postTags/useCases/add/CreatePostTagsUseCaseInterface';
+import { CreatePostTagsUseCase } from '@/application/contexts/postTags/useCases/add';
+import { GetPostTagsUseCaseInterface } from '@/application/contexts/postTags/useCases/get/GetPostTagsUseCaseInterface';
+import { GetPostTagsUseCase } from '@/application/contexts/postTags/useCases/get';
+import { PostTagsControllerInterface } from '@/infrastructure/api/controllers/interfaces/PostTagsControllerInterface';
+import { PostTagsController } from '@/infrastructure/api/controllers/postTagsController';
+import { CreatePostTagCategoryUseCaseInterface } from '@/application/contexts/postTagCategory/useCases/add/CreatePostTagCategoryUseCaseInterface';
 
 export class AppDependencyInjector {
   private static _dashboardControllerInstance: DashboardControllerInterface;
@@ -166,6 +191,36 @@ export class AppDependencyInjector {
     return this._mapsRepositoryInstance;
   }
 
+  private static _postTagCategoryRepositoryInstance: PostTagCategoryRepositoryInterface;
+
+  static get postTagCategoryRepository(): PostTagCategoryRepositoryInterface {
+    if (!this._postTagCategoryRepositoryInstance) {
+      this._postTagCategoryRepositoryInstance = new PostTagCategoryRepository();
+    }
+
+    return this._postTagCategoryRepositoryInstance;
+  }
+
+  private static _createPostTagCategoryUseCaseInstance: CreatePostTagCategoryUseCaseInterface;
+
+  static get createPostTagCategoryUseCase(): CreatePostTagCategoryUseCaseInterface {
+    if (!this._createPostTagCategoryUseCaseInstance) {
+      this._createPostTagCategoryUseCaseInstance = new CreatePostTagCategoryUseCase(this.postTagCategoryRepository);
+    }
+
+    return this._createPostTagCategoryUseCaseInstance;
+  }
+
+  private static _getPostTagCategoryUseCaseInstance: GetPostTagCategoryUseCaseInterface;
+
+  static get getPostTagCategoryUseCase(): GetPostTagCategoryUseCaseInterface {
+    if (!this._getPostTagCategoryUseCaseInstance) {
+      this._getPostTagCategoryUseCaseInstance = new GetPostTagCategoryUseCase(this.postTagCategoryRepository);
+    }
+
+    return this._getPostTagCategoryUseCaseInstance;
+  }
+
   private static _mapsControllerInstance: MapsControllerInterface;
 
   static get mapsController(): MapsControllerInterface {
@@ -174,6 +229,26 @@ export class AppDependencyInjector {
     }
 
     return this._mapsControllerInstance;
+  }
+
+  private static _createAgentUseCaseInstance: CreateAgentUseCaseInterface;
+
+  static get createAgentUseCase(): CreateAgentUseCaseInterface {
+    if (!this._createAgentUseCaseInstance) {
+      this._createAgentUseCaseInstance = new CreateAgentUseCase(this.agentsRepository);
+    }
+
+    return this._createAgentUseCaseInstance;
+  }
+
+  private static _getAgentsUseCaseInstance: GetAgentsUseCaseInterface;
+
+  static get getAgentsUseCase(): GetAgentsUseCaseInterface {
+    if (!this._getAgentsUseCaseInstance) {
+      this._getAgentsUseCaseInstance = new GetAgentsUseCase(this.agentsRepository);
+    }
+
+    return this._getAgentsUseCaseInstance;
   }
 
   private static _createMapUseCaseInstance: CreateMapUseCaseInterface;
@@ -194,6 +269,26 @@ export class AppDependencyInjector {
     }
 
     return this._getMapsUseCaseInstance;
+  }
+
+  private static _agentsRepositoryInstance: AgentsRepositoryInterface;
+
+  static get agentsRepository(): AgentsRepositoryInterface {
+    if (!this._agentsRepositoryInstance) {
+      this._agentsRepositoryInstance = new AgentsRepository();
+    }
+
+    return this._agentsRepositoryInstance;
+  }
+
+  private static _agentsControllerInstance: AgentsControllerInterface;
+
+  static get agentsController(): AgentsControllerInterface {
+    if (!this._agentsControllerInstance) {
+      this._agentsControllerInstance = new AgentsController(this.createAgentUseCase, this.getAgentsUseCase);
+    }
+
+    return this._agentsControllerInstance;
   }
 
   static get authController(): AuthControllerInterface {
@@ -330,6 +425,62 @@ export class AppDependencyInjector {
     return this._loginUseCaseInstance;
   }
 
+  private static _postTagCategoryControllerInstance: PostTagCategoryControllerInterface;
+
+  static get postTagCategoryController(): PostTagCategoryControllerInterface {
+    if (!this._postTagCategoryControllerInstance) {
+      this._postTagCategoryControllerInstance = new PostTagCategoryController(
+        this.createPostTagCategoryUseCase,
+        this.getPostTagCategoryUseCase,
+      );
+    }
+
+    return this._postTagCategoryControllerInstance;
+  }
+
+  private static _postTagsRepositoryInstance: PostTagsRepositoryInterface;
+
+  static get postTagsRepository(): PostTagsRepositoryInterface {
+    if (!this._postTagsRepositoryInstance) {
+      this._postTagsRepositoryInstance = new PostTagsRepository();
+    }
+
+    return this._postTagsRepositoryInstance;
+  }
+
+  private static _createPostTagsUseCaseInstance: CreatePostTagsUseCaseInterface;
+
+  static get createPostTagsUseCase(): CreatePostTagsUseCaseInterface {
+    if (!this._createPostTagsUseCaseInstance) {
+      this._createPostTagsUseCaseInstance = new CreatePostTagsUseCase(
+        this.postTagsRepository,
+        this.postTagCategoryRepository,
+      );
+    }
+
+    return this._createPostTagsUseCaseInstance;
+  }
+
+  private static _getPostTagsUseCaseInstance: GetPostTagsUseCaseInterface;
+
+  static get getPostTagsUseCase(): GetPostTagsUseCaseInterface {
+    if (!this._getPostTagsUseCaseInstance) {
+      this._getPostTagsUseCaseInstance = new GetPostTagsUseCase(this.postTagsRepository);
+    }
+
+    return this._getPostTagsUseCaseInstance;
+  }
+
+  private static _postTagsControllerInstance: PostTagsControllerInterface;
+
+  static get postTagsController(): PostTagsControllerInterface {
+    if (!this._postTagsControllerInstance) {
+      this._postTagsControllerInstance = new PostTagsController(this.createPostTagsUseCase, this.getPostTagsUseCase);
+    }
+
+    return this._postTagsControllerInstance;
+  }
+
   static get createPostUseCase(): CreatePostUseCaseInterface {
     if (!this._createPostUseCaseInstance) {
       this._createPostUseCaseInstance = new CreatePostUseCase(this.postRepository, this.userRepository);
@@ -340,7 +491,12 @@ export class AppDependencyInjector {
 
   static get updatePostUseCase(): UpdatePostUseCaseInterface {
     if (!this._updatePostUseCaseInstance) {
-      this._updatePostUseCaseInstance = new UpdatePostUseCase(this.postRepository, this.userRepository);
+      this._updatePostUseCaseInstance = new UpdatePostUseCase(
+        this.postRepository,
+        this.userRepository,
+        this.mapsRepository,
+        this.agentsRepository,
+      );
     }
 
     return this._updatePostUseCaseInstance;
